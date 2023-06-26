5da0a03cee0006443f1eb830e15db94392130ea9
leoshatrushin <u7302755@anu.edu.au>
2023-06-26 22:37:20 +0800

    Added files

---
+++ lists/src/second.rs
@@ -0,0 +1,80 @@
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem,
+            next : self.head.take(),
+        });
+
+        self.head = Some(new_node);
+    }
+
+    fn pop_node(&mut self) -> Link<T> {
+        self.head.take().map(|mut node| {
+            self.head = node.next.take();
+            node
+        })
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.pop_node().map(|node| { node.elem  })
+    }
+
+    pub fn peek(&mut self) -> T {
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop_node() {}
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
